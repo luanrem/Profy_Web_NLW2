6a318e6048565a791706b6f626b96745cc57a4d5
Luan <luanrem@gmail.com>
2020-08-05 17:20:13 -0300

    initial commit

---
+++ src/components/TeacherItem/index.tsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import whatsappIcon from '../../assets/images/icons/whatsapp.svg';
+
+import './styles.css';
+
+function TeacherItem() {
+  return (
+    <article className="teacher-item">
+          <header>
+            <img src="https://media-exp1.licdn.com/dms/image/C4E03AQGs0hszumPd8w/profile-displayphoto-shrink_100_100/0?e=1602115200&v=beta&t=PJ5fSPiTlBU2yqcXe94hFuRXw2JxuvRae5VgcPB0QBU" alt="Luan Martins"/>
+            <div>
+              <strong>Luan Martins</strong>
+              <span>Química</span>
+            </div>
+          </header>
+
+          <p>
+            Entusiasta das melhores tecnologias de química avançada.
+            <br/>
+            Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências
+
+          </p>
+
+          <footer>
+            <p>
+              Preço/hora
+              <strong>R$ 80,00</strong>
+            </p>
+            <button type="button"> 
+              <img src={whatsappIcon} alt="Whatsapp"/>
+              Entrar em contato
+            </button>
+          </footer>
+        </article>
+  );
+}
+
+export default TeacherItem;
